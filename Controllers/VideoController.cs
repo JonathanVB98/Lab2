0b6497ffcda2b733e0f86a68a58f1b88406b5b99
Jonathan Yair Vazquez Buenrostro <jon_j125@hotmail.com>
2020-03-06 18:51:40 -0600

    Agregar video terminado

---
+++ Controllers/VideoController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.Data;
+using MVCPlantilla.Utilerias;
+
+namespace MvcPlantilla.Controllers
+{
+    public class VideoController : Controller
+    {
+        //
+        // GET: /Video/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Agregar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Agregar(int idvideo, string titulo, int reproducciones, string link)
+        {
+            List<SqlParameter> parametros = new List<SqlParameter>();
+            parametros.Add(new SqlParameter("@idvideo", idvideo));
+            parametros.Add(new SqlParameter("@titulo", titulo));
+            parametros.Add(new SqlParameter("@reproducciones", reproducciones));
+            parametros.Add(new SqlParameter("@link", link));
+            BaseHelper.ejecutarSentencia("Sp_Agregar", CommandType.StoredProcedure, parametros);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Modificar()
+        {
+            return View();
+        }
+
+        public ActionResult Eliminar()
+        {
+            return View();
+        }
+    }
+}
---
+++ Views/Video/Agregar.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" Inherits="System.Web.Mvc.ViewPage<dynamic>" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>Agregar</title>
+</head>
+<body>
+    <form action="/Video/Agregar" method="post">
+        <fieldset>
+            <legend> Datos del video</legend>
+            
+            <label for="idvideo">Id Video</label>
+            <input type="text" name="idvideo"/>
+            <br />
+            
+            <label for="titulo">Titulo</label>
+            <input type="text" name="titulo"/>
+            <br />
+            
+            <label for="reproducciones">Reproducciones</label>
+            <input type="text" name="reproducciones"/>
+            <br />
+
+            <label for="link">Link</label>
+            <input type="text" name="link"/>
+
+            <input type="submit" value="Registrar" />        
+        </fieldset>
+    </form>
+</body>
+</html>
